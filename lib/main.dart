ae6d76353335b7b7f086d5f1a5e9e35cef41baf5
PhoenixKnight <phoenixknight18012007@gmail.com>
2025-06-22 13:05:03 +0530

    Statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:full_flutter_beginner/pages/first_page.dart';
-import 'package:full_flutter_beginner/pages/home_page.dart';
-import 'package:full_flutter_beginner/pages/profile_page.dart';
-import 'package:full_flutter_beginner/pages/settings_page.dart';
+import 'package:full_flutter_beginner/pages/counter_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FirstPage(),
-      routes: {
-        '/firstpage': (context) => FirstPage(),
-        '/homepage': (context) => HomePage(),
-        '/settingspage': (context) => SettingsPage(),
-        '/profilepage': (context) => ProfilePage(),
-      }
-
-
+      home: CounterPage(),
     );
   }
 }
